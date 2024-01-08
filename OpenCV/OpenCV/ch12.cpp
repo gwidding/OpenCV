a329ef42c5022b29a8cb689a45d3a81abf35aefc
gwidding <135992700+gwidding@users.noreply.github.com>
2024-01-08 14:17:49 +0900

    레이블링 이해, 응용

---
+++ OpenCV/OpenCV/ch12.cpp
@@ -0,0 +1,72 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+#if 1
+// 레이블링의 이해
+
+void labeling_basic() {
+
+	uchar data[] = {
+		0, 0, 1, 1, 0, 0, 0, 0,
+		1, 1, 1, 1, 0, 0, 1, 0,
+		1, 1, 1, 1, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 1, 1, 0,
+		0, 0, 0, 1, 1, 1, 1, 0,
+		0, 0, 0, 1, 0, 0, 1, 0,
+		0, 0, 1, 1, 1, 1, 1, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+	};
+
+	Mat src = Mat(8, 8, CV_8UC1, data) * 255;
+
+	Mat labels;
+	int cnt = connectedComponents(src, labels);
+
+	cout << "src:\n" << src << endl;
+	cout << "label:\n" << labels << endl;
+	cout << "number of labels: " << cnt << endl;
+}
+
+// 레이블링 응용
+void labeling_stats() {
+	Mat src = imread("C:/work/img/keyboard.bmp", IMREAD_GRAYSCALE);
+	if (src.empty()) {
+		cerr << "이미지 로드 실패" << endl;
+		return;
+	}
+
+	Mat bin;
+	threshold(src, bin, 0, 255, THRESH_BINARY | THRESH_OTSU);
+
+	Mat labels, stats, centroids;
+	int cnt = connectedComponentsWithStats(bin, labels, stats, centroids);
+
+	Mat dst;
+	cvtColor(src, dst, COLOR_GRAY2BGR);
+
+	for (int i = 1; i < cnt; i++) {
+		int* p = stats.ptr<int>(i);
+
+		if (p[4] < 20) continue;
+
+		rectangle(dst, Rect(p[0], p[1], p[2], p[3]), Scalar(0, 255, 255), 2);
+	}
+
+	imshow("src", src);
+	imshow("dst", dst);
+
+	waitKey();
+	destroyAllWindows();
+}
+
+int main() {
+	//labeling_basic();
+	labeling_stats();
+
+	return 0;
+}
+
+#endif
